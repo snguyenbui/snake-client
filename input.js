f0b2f08fb6432a0850c4ba0f0bff2e51eb87cf94
Sunny Nguyen-Bui <snguyenbui@gmail.com>
2021-03-10 22:36:17 +0000

    removed setupInput and handleUserInput from play.js and created input.js for them

---
+++ input.js
@@ -0,0 +1,22 @@
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+ const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.on("data", (key) => {
+    handleUserInput(key)
+  })
+  stdin.resume();
+  return stdin;
+}
+
+const handleUserInput = (key) => {
+  if (key === '\u0003') {
+    process.exit();
+  }
+}
+
+module.exports = setupInput;
--- play.js
+++ play.js
@@ -1,29 +1,10 @@
+const setupInput = require('./input')
+const connect = require('./client');
+
 /**
  * Establish connection to the server
  */
-const connect = require('./client');
 console.log('Connecting ...');
 connect();
-
-/**
- * Setup User Interface 
- * Specifically, so that we can handle user input via stdin
- */
- const setupInput = () => {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.on("data", (key) => {
-    handleUserInput(key)
-  })
-  stdin.resume();
-  return stdin;
-}
-
-const handleUserInput = (key) => {
-  if (key === '\u0003') {
-    process.exit();
-  }
-}
 
 setupInput();
