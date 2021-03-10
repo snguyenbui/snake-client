bba40f67d47a341e215457a2ff1eff51690ee6df
Sunny Nguyen-Bui <snguyenbui@gmail.com>
2021-03-10 21:28:48 +0000

    linted and added connection confirmation and sending a name

--- client.js
+++ client.js
@@ -1,14 +1,19 @@
 const net = require('net');
 
 const connect = () => {
-  const conn = net.createConnection({ 
+  const conn = net.createConnection({
     host: 'localhost',
     port: 50541
   });
   // interpret incoming data as text
-  conn.setEncoding('utf8'); 
+  conn.setEncoding('utf8');
+
+  conn.on("connect", () => {
+    console.log("Connection established");
+    conn.write("Name: SUN");
+  });
 
   return conn;
-}
+};
 
 module.exports = connect;
